fd3f5235a5a400cc7ae512ef56576e9e32232f86
lauraS-mda <elena.esther.dedios@gmail.com>
2020-08-10 10:37:51 -0300

    listProvincias

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Provincia;
+
 /**
  * @Route("/api", name="api")
  */
@@ -11,12 +15,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiController extends AbstractController
 {
     
+   
     /**
-    * @Route("/provincias", name="list_provincias")
-    */
-    public function listProvincias()
-    {
-        $this->getDoctrine()->getManager
-        return new JsonResponse();
-    }
+    * @Route("/provincias", name="_list_provincias", methods={"GET"})
+   */
+  public function listProvincias()
+  {
+      $pcias=$this->getDoctrine()->getManager()->getRepository(Provincia::class)->findAll();
+      return new jsonResponse($pcias);
+  }
 }
