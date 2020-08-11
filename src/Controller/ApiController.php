54a1b8f726dbd4529830eb0b2dc5ea08e528f7dc
lauraS-mda <elena.esther.dedios@gmail.com>
2020-08-11 09:07:28 -0300

    partidos

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -18,19 +18,19 @@ class ApiController extends AbstractController
     
    
     /**
-    * @Route("/provincias", name="_list_provincias", methods={"GET"})
+    * @Route("/provincias", name="_get_provincias", methods={"GET"})
    */
-  public function listProvincias()
+  public function getProvincias()
   {
       $pcias=$this->getDoctrine()->getManager()->getRepository(Provincia::class)->findAll();
       return new jsonResponse($pcias);
   }
 
     /**
-     * @Route("/provincias/{id}", name="_buscar_pciaxid", methods={"GET"})
+     * @Route("/provincias/{id}", name="_get_pciaxid", methods={"GET"})
      */
     //buscar por un parametro
-    public function buscarPciaxid($id)
+    public function getPciaxid($id)
     {
         
         $pcia=$this->getDoctrine()->getManager()->getRepository(Provincia::class)->find($id);
@@ -41,17 +41,49 @@ class ApiController extends AbstractController
         return new jsonResponse($pcia);
        
       
-    }  
+    } 
+
+     /**
+     * @Route("/provincia", name="_get_pciaxnom", methods={"GET"})
+     */
+    //si dejo provincias no me anda y no entiendo porque?
+    public function getPciaxnom(Request $request)
+    {
+        $nombre=$request->query->get('nombre');
+        $query=['nombre'=>$nombre];
+
+        $pcia=$this->getDoctrine()->getManager()->getRepository(Provincia::class)->findBy($query);
+        if (is_null($pcia)){
+            throw $this->createNotFoundException();
+
+        }
+        return new jsonResponse($pcia);
+       
+      
+    } 
+
+    
+    /**
+    * @Route("/partidos", name="_get_partidos", methods={"GET"})
+    */
+  public function getPartidos()
+  {
+      $partidos=$this->getDoctrine()->getManager()->getRepository(Partido::class)->findAll();
+      return new jsonResponse($partidos);
+  }
 
     /**
-     * @Route("/partidosxpcia/{id}", name="_list_partidos", methods={"GET"})
+     * @Route("/partidos", name="_get_partidosxid", methods={"GET"})
      */
-    //buscar por un parametro
-    public function listPartidos($id)
+    //buscar por un parametro idpcia
+    public function getPartidosxid(Request $request)
     {
         
-        //noo se como decirle que el id es el de provincia ¿?
-        $partidos=$this->getDoctrine()->getManager()->getRepository(Partido::class)->findBy( array(), array('nombre' => 'ASC') );
+        $provincia=$request->query->get('idpcia'); 
+        $query=['provincia'=>$provincia];
+        
+       
+        $partidos=$this->getDoctrine()->getManager()->getRepository(Partido::class)->findBy($query, array('nombre' => 'ASC'));   
         if (is_null($partidos)){
             throw $this->createNotFoundException();
 
@@ -59,5 +91,33 @@ class ApiController extends AbstractController
         return new jsonResponse($partidos);
        
       
+    } 
+    
+ /**
+     * @Route("/partido", name="_get_partido", methods={"GET"})
+     */
+    //buscar por un parametro idpcia,idpartido
+    public function getPartido(Request $request)
+    {
+        
+        $provincia=$request->query->get('idpcia'); 
+        $partido=$request->query->get('idpartido');
+
+        $query=[
+            'provincia'=>$provincia,
+            'id'=>$partido
+          ];
+        
+         
+        $partido=$this->getDoctrine()->getManager()->getRepository(Partido::class)->findBy($query);   //findBy( array(), array('nombre' => 'ASC') );
+        if (is_null($partido)){
+            throw $this->createNotFoundException();
+
+        }
+        return new jsonResponse($partido);
+       
+      
     }  
+ 
+
 }
